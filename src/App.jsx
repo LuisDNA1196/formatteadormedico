418b144dc95a3ead2ce8bfd50176bf4d68b68928
LuisDNA1196 <127688214+LuisDNA1196@users.noreply.github.com>
2024-05-18 12:01:50 -0600

    router2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-
-import {BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./components/Home"
-import { FormularioEmpleo } from "./components/FormularioEmpleo"
-import { ChakraProvider } from '@chakra-ui/react'
-import { FormularioPresent } from "./components/FormularioPresent"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import { FormularioEmpleo } from "./components/FormularioEmpleo";
+import { ChakraProvider } from "@chakra-ui/react";
+import { FormularioPresent } from "./components/FormularioPresent";
 
 function App() {
-
   return (
-    <BrowserRouter>
-  <ChakraProvider>
-      
-    <Routes >
-      <Route path="/" element={<Home/>}/>
-      <Route path="FormularioPresent" element={<FormularioPresent/>} />
-      <Route path="FormularioEmpleo" element={<FormularioEmpleo/>} />
-    </Routes>
-
-
-  </ChakraProvider>
-    </BrowserRouter>
-  )
+    <Router>
+      <ChakraProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="FormularioPresent" element={<FormularioPresent />} />
+          <Route path="FormularioEmpleo" element={<FormularioEmpleo />} />
+        </Routes>
+      </ChakraProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
